a4006701d2c05c53d80af0e026f2b7c31315a821
Sarah Usher <sarahnusher@gmail.com>
2020-08-28 18:58:16 +0100

    added an option to run an example

--- main.py
+++ main.py
@@ -1,11 +1,21 @@
 from translator_service import TranslatorService
 
+translator_service = TranslatorService()
+
+
+def run_example():
+    in_text = "3 :thumbsup: -n 2 -s *"
+    print("Example input:", in_text)
+    print("Output:", translator_service.run_translation(in_text))
+
 
 def main():
-    user_input = input("Input emoji codes. E to exit: ")
+    user_input = input("Input emoji codes. E to exit. X for example: ")
     while user_input != "E":
-        translator_service = TranslatorService()
-        print(translator_service.run_translation(user_input))
+        if user_input == "X":
+            run_example()
+        else:
+            print(translator_service.run_translation(user_input))
         user_input = input("Input emoji codes. E to exit: ")
 
 
